b233fb3646263d7f72c85ea9b43030e717b17cba
Dongkyoo <wind.dong.dream@gmail.com>
2019-07-22 20:45:07 +0900

    Shell 발사 구현

---
+++ Assets/Scripts/Shell/Shell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.forward * 1.0f);
+    }
+}
