9c15e9855b7a33bb1ddf5fb8983efa300b971596
HamInKyou <50827930+HamInKyou@users.noreply.github.com>
2019-07-23 23:45:44 +0900

    tank 움직임 더 구현

--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -2,40 +2,35 @@
 
 public class TankMovement : MonoBehaviour
 {
-    public int m_PlayerNumber = 1;         
-    public float m_Speed = 12f;            
-    public float m_TurnSpeed = 180f;       
-    public AudioSource m_MovementAudio;    
-    public AudioClip m_EngineIdling;       
-    public AudioClip m_EngineDriving;      
-    public float m_PitchRange = 0.2f;
+    public int m_PlayerNumber = 1;         //플레이어 넘버
+    public float m_Speed = 12f;            //이동속도
+    public float m_TurnSpeed = 180f;       //회전속도
 
-    /*
-    private string m_MovementAxisName;     
-    private string m_TurnAxisName;         
-    private Rigidbody m_Rigidbody;         
-    private float m_MovementInputValue;    
-    private float m_TurnInputValue;        
-    private float m_OriginalPitch;         
+    
+    private string m_MovementAxisName;     //앞뒤 축
+    private string m_TurnAxisName;         //회전 축
+    private Rigidbody m_Rigidbody;         //컴포넌트 담는거
+    private float m_MovementInputValue;    //앞뒤
+    private float m_TurnInputValue;        //회전
 
 
     private void Awake()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Rigidbody = GetComponent<Rigidbody>(); //컴포넌트 rigidbody 갖고오기, rigidbody는 게임 오브젝트가 물리엔진의 영향을 받게 만드는 기능
     }
 
 
     private void OnEnable ()
     {
-        m_Rigidbody.isKinematic = false;
-        m_MovementInputValue = 0f;
-        m_TurnInputValue = 0f;
+        m_Rigidbody.isKinematic = false; //true일 경우 물리엔진에 의해서는 이동하지 않고 애니메이션이나 Transform컴포넌트를 통해서만 이동
+        m_MovementInputValue = 0f; //초기화
+        m_TurnInputValue = 0f; //초기화
     }
 
 
     private void OnDisable ()
     {
-        m_Rigidbody.isKinematic = true;
+        m_Rigidbody.isKinematic = true; //물리엔진의 영향을 받지 않는다.
     }
 
 
@@ -43,37 +38,42 @@ public class TankMovement : MonoBehaviour
     {
         m_MovementAxisName = "Vertical" + m_PlayerNumber;
         m_TurnAxisName = "Horizontal" + m_PlayerNumber;
-
-        m_OriginalPitch = m_MovementAudio.pitch;
     }
-    */
+    
 
-    private void Update()
+    private void Update() //입력받는거
     {
-        // Store the player's input and make sure the audio for the engine is playing.
-    }
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName); //앞뒤
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName); //회전
 
-
-    private void EngineAudio()
-    {
-        // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
     }
 
-
-    private void FixedUpdate()
+    private void FixedUpdate() //업데이트대로 실행
     {
-        // Move and turn the tank.
+        Move();
+        Turn();
     }
 
 
     private void Move()
     {
-        // Adjust the position of the tank based on the player's input.
+        //탱크 움직이는 벡터값 입력
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        //rigidbody를 옮겨준다
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
     }
 
 
     private void Turn()
     {
-        // Adjust the rotation of the tank based on the player's input.
+        //탱크 회전시키는 값 입력
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        //회전시켜줘
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        //rigidbody에도 적용시커줘
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
     }
 }
