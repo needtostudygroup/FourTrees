2428a1d1fe6f949bc39e289d5dab13897eefa6d5
Dongkyoo <wind.dong.dream@gmail.com>
2019-07-24 15:55:53 +0900

    Shell 카메라 시점을 좀 더 먼 거리로 수정
    너무 가까이 설정되어서 파워가 세지면 Shell 시점의 카메라가 탱크를 바라보는 버그가 있었음

--- Assets/Scripts/Shell/Shell.cs
+++ Assets/Scripts/Shell/Shell.cs
@@ -6,28 +6,34 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     private Rigidbody rigidbody;
+    
+    // Shell을 Tracking 하는 카메라
     private Camera camera;
     
+    // Shell 발포 방향
     public Vector3 direction;
+    
+    // Shell 발포 세기
     public float power;
 
-    // Shell 은 렌더링과 동시에 주어진 direction과 power 로 발사된됨
     private void Start()
     {
-        rigidbody = this.GetComponent<Rigidbody>();
+        // Shell 은 렌더링과 동시에 주어진 direction과 power 로 발사됨
+        rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(direction * power);
 
-        camera = this.GetComponentInChildren<Camera>();
+        camera = GetComponentInChildren<Camera>();
     }
 
     private void FixedUpdate()
     {
-        camera.transform.LookAt(rigidbody.velocity);
+        // 포탄 시점으로 카메라 전환
+        camera.transform.LookAt(rigidbody.velocity * 100);
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        // HitableObject 를 만나면 일어나는 반은
+        // HitableObject 를 만나면 오브젝트 사라짐
         if (other.collider.tag == GlobalVariable.HitableObject)
         {
             Destroy(gameObject);
