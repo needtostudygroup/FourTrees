53c3a43fe29bd46a1d1734c3f68c8ce79e75b995
JungHyun <f7817455@naver.com>
2019-07-27 23:52:09 +0900

    Fire 수정ver3

--- Assets/Scripts/Shell/Fire.cs
+++ Assets/Scripts/Shell/Fire.cs
@@ -8,21 +8,21 @@ public class Fire : MonoBehaviour
 {
     private const float MIN_POWER = 500.0f;
     private const float MAX_POWER = 3000.0f;
-    public float CurrentPower;
-    private const float PushTime = 0.9f;
-    private bool IsSpacePressed = false;
+    public float currentPower;
+    private const float PUSHTIME = 0.9f;
+    private bool isSpacePressed = false;
     public Transform firePosition;
     private float chargeSpeed;
-    public Slider PowerGage;
+    public Slider powerGage;
     public Shell shell;
 
     // Start is called before the first frame update
     void Start()
     {
-        CurrentPower = MIN_POWER;
-        PowerGage.maxValue = MAX_POWER;
-        PowerGage.value = CurrentPower;
-        chargeSpeed = (MAX_POWER - MIN_POWER) / PushTime;
+        currentPower = MIN_POWER;
+        powerGage.maxValue = MAX_POWER;
+        powerGage.value = currentPower;
+        chargeSpeed = (MAX_POWER - MIN_POWER) / PUSHTIME;
     }
 
 
@@ -33,25 +33,25 @@ public class Fire : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) == true)
         {
             shell.direction = this.transform.forward + new Vector3(0, 1.0f, 0);
-            IsSpacePressed = true;
+            isSpacePressed = true;
         }
         //released spcae bar
         else if (Input.GetKeyUp(KeyCode.Space))
         {
-            if (CurrentPower >= MAX_POWER)
-                CurrentPower = MAX_POWER;
-            shell.power = CurrentPower;
+            if (currentPower >= MAX_POWER)
+                currentPower = MAX_POWER;
+            shell.power = currentPower;
             Instantiate(shell, firePosition.position, firePosition.rotation);
-            IsSpacePressed = false;
+            isSpacePressed = false;
             //reset
-            CurrentPower = MIN_POWER;
+            currentPower = MIN_POWER;
             shell.power = MIN_POWER;
         }
         //space bar is being pressed
-        if (IsSpacePressed)
+        if (isSpacePressed)
         {
-            CurrentPower += chargeSpeed * Time.deltaTime;
-            PowerGage.value = CurrentPower;
+            currentPower += chargeSpeed * Time.deltaTime;
+            powerGage.value = currentPower;
         }
     }
 }
