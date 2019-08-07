8fbbca1d7dabbd123f7b6e7c575bfa82837b3474
Dongkyoo <wind.dong.dream@gmail.com>
2019-08-07 23:25:09 +0900

    탱크 터렛이 마우스에 따라 안움직이던 버그 수정

--- Assets/InitTerrain.cs
+++ Assets/InitTerrain.cs
@@ -7,8 +7,8 @@ public class InitTerrain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        RenderTexture cursorSplat = new RenderTexture(1024 * 1, 1024 * 1, 0, RenderTextureFormat.ARGBFloat);
-        RenderTexture terrainChangedMap = new RenderTexture(1024 * 1, 1024 * 1, 0, RenderTextureFormat.RFloat);
+        RenderTexture cursorSplat = new RenderTexture(1024 * 10, 1024 * 10, 0, RenderTextureFormat.ARGBFloat);
+        RenderTexture terrainChangedMap = new RenderTexture(1024 * 2, 1024 * 2, 0, RenderTextureFormat.RFloat);
         Material material = GetComponent<Terrain>().materialTemplate;
         
         material.SetTexture("_CursorSplat", cursorSplat);
